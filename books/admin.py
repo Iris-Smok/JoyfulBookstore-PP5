a15495728de5787717d9f785b66b1ecf69e2c542
Iris-Smok <irissmok11@gmail.com>
2022-07-25 22:06:27 +0000

    Add images to book database

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from .models import Book, Category
 
-admin.site.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    """ product model display """
+    list_display = (
+        'title',
+        'category',
+        'image'
+    )
+
+
+ordering = ('category',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    """category model dissplay"""
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
+admin.site.register(Book, BookAdmin)
 admin.site.register(Category)
