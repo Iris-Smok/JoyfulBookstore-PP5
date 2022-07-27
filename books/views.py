94450d03aecc9b0034e0c6d2f8f7b006e7dd41ba
Iris-Smok <irissmok11@gmail.com>
2022-07-27 14:23:23 +0000

    Create view for all books page

--- books/views.py
+++ books/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+
+def all_books(request):
+    """ A view to show all books, including sorting and searching queries"""
+
+    books = Book.objects.all()
+    context = {
+        'books': books,
+    }
+    return render(request, 'books/books.html', context)
