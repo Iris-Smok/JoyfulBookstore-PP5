e7ca2a0ad1f00eea69d399c058bc18ea5fb660b6
Iris-Smok <irissmok11@gmail.com>
2022-07-28 22:53:07 +0000

    Create view, url, css and template + include for book detail page

--- books/urls.py
+++ books/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_books, name='books'),
+    path('<int:book_id>/', views.book_detail, name='book_detail'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Book
 
 
@@ -10,3 +10,13 @@ def all_books(request):
         'books': books,
     }
     return render(request, 'books/books.html', context)
+
+
+def book_detail(request, book_id):
+    """ A view to show book details"""
+
+    book = get_object_or_404(Book, pk=book_id)
+    context = {
+        'book': book,
+    }
+    return render(request, 'books/book_detail.html', context)
