50bbaaa4e53ddc719b7d58de53cb58c4e2c6005d
Iris-Smok <irissmok11@gmail.com>
2022-07-31 00:31:26 +0000

    Added search functionallity

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Book
 
 
@@ -6,8 +8,21 @@ def all_books(request):
     """ A view to show all books, including sorting and searching queries"""
 
     books = Book.objects.all()
+    query = None
+
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.error(
+                request, "You didn't enter any search criteria!")
+            return redirect(reverse('books'))
+
+        queries = Q(
+            title__icontains=query) | Q(description__icontains=query)
+        books = books.filter(queries)
     context = {
         'books': books,
+        'search_term': query,
     }
     return render(request, 'books/books.html', context)
 
--- joyfulbookstore/settings.py
+++ joyfulbookstore/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
     },
 ]
 
+
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
