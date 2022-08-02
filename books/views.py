674b9c4cb66bc035c010b1d19707db4add4a149e
Iris-Smok <irissmok11@gmail.com>
2022-08-02 14:50:10 +0000

    Add sort box functionality

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Book, Category
 
 
@@ -10,8 +11,25 @@ def all_books(request):
     books = Book.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                books = books.annotate(lower_title=Lower('title'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            books = books.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             books = books.filter(category__name__in=categories)
@@ -28,10 +46,13 @@ def all_books(request):
             title__icontains=query) | Q(description__icontains=query)
         books = books.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'books': books,
         'search_term': query,
         'current_categories': categories,
+        "current_sorting": current_sorting,
     }
     return render(request, 'books/books.html', context)
 
