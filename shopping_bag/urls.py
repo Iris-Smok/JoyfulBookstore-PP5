af3c53faeaee599389b3bf10c1afa35d4dff3a11
Iris-Smok <irissmok11@gmail.com>
2022-08-04 13:34:05 +0000

    Add forum for quantity and shopping bag,input btns. Add add to shopping_bag functionality

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.shopping_bag, name='shopping_bag'),
+    path('', views.shopping_bag_view, name='shopping_bag'),
+    path('add/<item_id>', views.add_to_shopping_bag, name='add_to_shopping_bag'),
 ]
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
-def shopping_bag(request):
+def shopping_bag_view(request):
+    """ A view that render the shopping_bag content"""
     return render(request, 'shopping_bag/shopping_bag.html')
+
+
+def add_to_shopping_bag(request, item_id):
+    """ Add a quantity of the book to the shopping bag"""
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    if item_id in list(shopping_bag.keys()):
+        shopping_bag[item_id] += quantity
+    else:
+        shopping_bag[item_id] = quantity
+
+    request.session['shopping_bag'] = shopping_bag
+    print(request.session['shopping_bag'])
+    return(redirect(redirect_url))
