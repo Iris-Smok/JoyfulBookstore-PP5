6fc34ffe68f8df9fd20d724a9a709ccc0e691e82
Iris-Smok <irissmok11@gmail.com>
2022-08-04 22:44:14 +0000

    Added quantity input buttons, change book_id to item_id

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -6,17 +6,17 @@ def shopping_bag_view(request):
     return render(request, 'shopping_bag/shopping_bag.html')
 
 
-def add_to_shopping_bag(request, book_id):
+def add_to_shopping_bag(request, item_id):
     """ Add a quantity of the book to the shopping bag"""
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     shopping_bag = request.session.get('shopping_bag', {})
 
-    if book_id in list(shopping_bag.keys()):
-        shopping_bag[book_id] += quantity
+    if item_id in list(shopping_bag.keys()):
+        shopping_bag[item_id] += quantity
     else:
-        shopping_bag[book_id] = quantity
+        shopping_bag[item_id] = quantity
 
     request.session['shopping_bag'] = shopping_bag
     return(redirect(redirect_url))
