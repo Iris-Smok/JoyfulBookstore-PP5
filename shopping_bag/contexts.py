8dbd508f39b57715ce57efcec5faceda1817171b
Iris-Smok <irissmok11@gmail.com>
2022-08-04 17:49:05 +0000

    Update shopping bag template and add link to quick add to bag btn on all books page

--- shopping_bag/contexts.py
+++ shopping_bag/contexts.py
@@ -19,12 +19,12 @@ def shopping_bag_contents(request):
         book_count += quantity
         shopping_bag_items.append({
             'item_id': item_id,
-            'qunatity': quantity,
+            'quantity': quantity,
             'book': book,
         })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.shopping_bag_view, name='shopping_bag'),
-    path('add/<item_id>', views.add_to_shopping_bag, name='add_to_shopping_bag'),
+    path('add/<book_id>', views.add_to_shopping_bag, name='add_to_shopping_bag'),
 ]
