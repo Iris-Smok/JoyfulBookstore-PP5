c7fe5cea10a0bfb344acf14b5fcdc050b39bc8a4
Iris-Smok <irissmok11@gmail.com>
2022-08-06 17:23:51 +0000

    Added adjust bag and remove,delete functionality and calc subtotal

--- shopping_bag/urls.py
+++ shopping_bag/urls.py
@@ -4,6 +4,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.shopping_bag_view, name='shopping_bag'),
+    path('', views.shopping_bag_view, name='shopping_bag_view'),
     path('add/<item_id>', views.add_to_shopping_bag, name='add_to_shopping_bag'),
+    path('adjust/<item_id>', views.adjust_shopping_bag,
+         name='adjust_shopping_bag'),
+    path('delete/<item_id>/', views.delete_from_shopping_bag,
+         name='delete_from_shopping_bag'),
 ]
--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.contrib import messages
+from books.models import Book
 
 
 def shopping_bag_view(request):
@@ -20,3 +22,34 @@ def add_to_shopping_bag(request, item_id):
 
     request.session['shopping_bag'] = shopping_bag
     return(redirect(redirect_url))
+
+
+def adjust_shopping_bag(request, item_id):
+    """ Ajustthe quantity of the book to the specified amount"""
+
+    book = get_object_or_404(Book, pk=item_id)
+    quantity = int(request.POST.get('quantity') or 1)
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    if quantity > 99:
+        messages.error(
+            request, 'Sorry, value must be less then or equal to 99.')
+    elif quantity > 0:
+        shopping_bag[item_id] = quantity
+        messages.success(
+            request, f'Updated {book.title} quantity to {shopping_bag[item_id]}')
+    else:
+        shopping_bag.pop(item_id)
+        messages.success(request, f'Removed {book.title} from your bag')
+    request.session['shopping_bag'] = shopping_bag
+    return redirect(reverse('shopping_bag_view'))
+
+
+def delete_from_shopping_bag(request, item_id):
+    """ Removes item from bag """
+    book = get_object_or_404(Book, pk=item_id)
+    shopping_bag = request.session.get('shopping_bag', {})
+    shopping_bag.pop(item_id)
+    messages.success(request, f'Removed {book.title} from your bag')
+    request.session['shopping_bag'] = shopping_bag
+    return HttpResponse(status=200)
