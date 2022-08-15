a89945c8c98f68ac0f3050f662b1474a0131a71f
Iris-Smok <irissmok11@gmail.com>
2022-08-15 10:00:54 +0000

    Added update template

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
@@ -16,10 +17,17 @@ def profile(request):
     profile = get_object_or_404(UserProfile, user=request.user)
     form = UserProfileForm(instance=profile)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile update successfully!')
+
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'profile': profile,
+        'on_profile_page': True,
     }
     return render(request, template, context)
 
