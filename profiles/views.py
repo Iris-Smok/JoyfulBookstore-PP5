cb7951e58f78f77045d59ea87d0cca30b69fef6d
Iris-Smok <irissmok11@gmail.com>
2022-08-15 09:38:39 +0000

    Update accounts pages, create form for delivery details and order history view

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def account_overview(request):
@@ -11,9 +12,22 @@ def account_overview(request):
 
 def profile(request):
     """ Display the account overview"""
+
     profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=profile)
+
     template = 'profiles/profile.html'
     context = {
+        'form': form,
         'profile': profile,
     }
+    return render(request, template, context)
+
+
+def order_history(request):
+    """ Display order history """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.objects.all()
+    template = 'profiles/order_history'
+    context = {}
     return render(request, template, context)
