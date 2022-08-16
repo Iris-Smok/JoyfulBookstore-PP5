61d3e652700c75a79f5c6ea26da82824718f5ff6
Iris-Smok <irissmok11@gmail.com>
2022-08-16 15:03:20 +0000

    Create Category form and correct book form

--- books/forms.py
+++ books/forms.py
@@ -2,16 +2,24 @@ from django import forms
 from .models import Book, Category
 
 
+class CategoryForm(forms.ModelForm):
+    """ Category form"""
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+
 class BookForm(forms.ModelForm):
+    """ Book Form """
     class Meta:
         model = Book
-        fields = '__all__'
+        exclude = ('discount', 'rating', 'add_to_whishlist', 'review_count',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        friendly_name = [(c.id, c.get_friendly_name()) for c in categories]
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
-        self.fields['category'].choice = friendly_name
+        self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
