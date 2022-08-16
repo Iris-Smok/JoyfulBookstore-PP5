111e3ec4821f426544776fc222becfaba8044fd9
Iris-Smok <irissmok11@gmail.com>
2022-08-16 10:51:29 +0000

    Create Book form for Book Amin

---
+++ books/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Book, Category
+
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Category.objects.all()
+        friendly_name = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choice = friendly_name
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'border-black rounded-0'
