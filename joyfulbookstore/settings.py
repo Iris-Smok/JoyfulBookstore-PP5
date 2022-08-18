7429c069f59c69f3074fd18d3730dfc5a586fd45
Iris-Smok <irissmok11@gmail.com>
2022-08-18 13:26:32 +0000

    Removed secret key and set debug

--- joyfulbookstore/settings.py
+++ joyfulbookstore/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d$^7@g@^u@esx*^2qws1d=$(4jr0)m3x0og=06+=*8gvr6+$z#'  # noqa
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['joyfulbookstore.herokuapp.com', 'localhost']
 
