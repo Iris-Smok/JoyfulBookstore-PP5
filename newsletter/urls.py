6dcc094d3ce33de5cc65d10418ecd519b5c711b9
Iris-Smok <irissmok11@gmail.com>
2022-08-24 20:06:33 +0000

    Added send newsletter functionality, install django pandas

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.newsletter, name='newsletter'),
     path('subscribe_form_post', views.subscribe_form_post, name='subscribers'),
+    path('newsletter_email', views.newsletter_email, name='newsletter_email'),
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.core.mail import send_mail
+from django_pandas.io import read_frame
 from .models import Subscriber, SubscriberEmail
 from .forms import EmailForm, SubscriberForm
 
@@ -32,3 +34,35 @@ def subscribe_form_post(request):
                 subscribe_form.save()
                 messages.success(request, 'Successfully Subscribed.')
         return redirect(reverse("home"))
+
+
+def newsletter_email(request):
+    """ A view to allow superusers to send an email to their subscriber list """
+    emails = Subscriber.objects.all()
+    dataframe = read_frame(emails, fieldnames=['email'])
+    mail_list = dataframe['email'].values.tolist()
+    if request.method == "POST":
+        form = EmailForm(request.POST)
+        if form.is_valid:
+            form.save()
+            title = form.cleaned_data.get('title')
+            message = form.cleaned_data.get('message')
+            send_mail(
+                title,
+                message,
+                '',
+                mail_list,
+                fail_silently=False,
+            )
+            messages.success(request,
+                             'You Have Sent Your Newsletter \
+                              To Your Subscribers.')
+            return redirect(reverse("newsletter"))
+            print(form)
+            print(mail_list)
+    else:
+        form = EmailForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'newsletter/newsletter.html', context)
