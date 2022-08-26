940d6221555570242964fba67f64c91f878f82d8
Iris-Smok <irissmok11@gmail.com>
2022-08-26 16:39:30 +0000

    Write tests for home page and contact form view

---
+++ home/tests_views.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+
+class TestView(TestCase):
+    """ Test for views home and contact page page"""
+
+    def test_get_home_page(self):
+        """ test to check that home page is diplayed"""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/index.html')
+
+    def test_get_contact_page(self):
+        """ test to check that home page is diplayed"""
+        response = self.client.get('/contact/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/contact.html')
